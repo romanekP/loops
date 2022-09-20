231c7bd1bec00d4204dbda151991a867d0719b71
romanekP <romanekpawel@gmail.com>
2022-09-20 19:49:01 +0200

    Loop command (4 loops)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //For loop
+        String[] cars=new String[]{"Audi", "BMW", "Mercedes", "Toyota"};
+        for (int i=0; i<4; i++){
+            System.out.println(cars[i]);
+        }
     }
 }
