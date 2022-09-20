18664c04baa0737d46d5bbc97173d12b594a52d1
romanekP <romanekpawel@gmail.com>
2022-09-20 20:12:43 +0200

    added do while loop

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,11 @@
+public class DoWhileLoop  {
+    public static void main(String[] args) {
+
+        int x=0;
+        do{
+            ++x;
+            System.out.println(x);
+
+        }while(x<10);
+    }
+}
