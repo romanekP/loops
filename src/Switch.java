e0c254c83fb067f52abb0c79907b65614934d650
romanekP <romanekpawel@gmail.com>
2022-09-20 20:36:40 +0200

    added switch loop

--- src/Switch.java
+++ src/Switch.java
@@ -1,12 +1,13 @@
 public class Switch {
-    public static void main(String[] args){
-        int x=2;
-        switch (x){
+    public static void main(String[] args) {
+        int x = 2;
+        switch (x) {
             case 1:
                 System.out.println("Roman jest super!");
+                break;
             case 2:
                 System.out.println("Roman jest super mega!");
                 break;
         }
-}
+    }
 }
